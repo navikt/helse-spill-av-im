f3d9310cb01fede7f8ac60a4aa979c66ebc60e95
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-22 10:53:42 +0200

    Bumper r&r
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
+val rapidsAndRiversVersion = "2025092210081758528498.1d4b108f2c61"
 val flywayCoreVersion = "11.5.0"
 val hikariCPVersion = "6.3.0"
 val postgresqlVersion = "42.7.7"
